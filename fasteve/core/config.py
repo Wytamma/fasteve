8ccdabd8c2e108b397cb9b8446972bd04db999d4
Wytamma Wirth <wytamma@Wytammas-MacBook-Pro.local>
2019-08-24 13:33:12 +1000

    :tada: config

---
+++ fasteve/core/config.py
@@ -0,0 +1,9 @@
+from starlette.config import Config
+
+# Config will be read from environment variables and/or ".env" files.
+config = Config(".env")
+
+INFO = config('INFO', cast=str, default='_info')
+API_VERSION = config('API_VERSION', cast=str, default='0.1')
+HATEOAS = config('HATEOAS', cast=bool, default=True)
+LINKS = config('LINKS', cast=str, default='_links')
