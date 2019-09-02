a73c5ba3b35290309c0a99b386ae42fb0db4bbe7
Wytamma Wirth <wytamma@Wytammas-MacBook-Pro.local>
2019-09-02 22:47:14 +1000

    :tada: Added .core as default config

--- fasteve/aplications.py
+++ fasteve/aplications.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI
 from .middleware.setting import SettingsMiddleware
 from .endpoints import collections_endpoint, home_endpoint
-
+from .core import config as default_config
 
 class Fasteve(FastAPI):
     def __init__(self, settings: dict = {"DOMAIN": {}}) -> None:
 
         super().__init__()  # Instialise FastAPI super class
-
+        
+        # set setting defualts
+        # load user settings
+        # validate user settings
         self.settings = settings
-        self._register_settings_middleware()
+        self.config = default_config
+        if type(self.settings) is str:
+            self.load_user_config_from_file(self.settings)
 
+        self._validate_settings()
+        self._register_settings_middleware()
+        
         self._register_home_endpoint()
         
         domain = self.settings["DOMAIN"]
-
         for resource, resource_settings in domain.items():
             self.register_resource(resource, resource_settings)
 
@@ -28,8 +35,7 @@ class Fasteve(FastAPI):
         self.add_api_route(f"/", home_endpoint)
 
     def _register_settings_middleware(self) -> None:
-        """Pass validated settings to every request"""
-        self._validate_settings()
+        """Pass settings to every request"""
         self.add_middleware(SettingsMiddleware, settings=self.settings)
 
     def _validate_settings(self):
